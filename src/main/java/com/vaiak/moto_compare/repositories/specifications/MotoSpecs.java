cfee47e0f4a5454b3a99431c9d8b9e9cf6148d4a
vasileiosiakovidis <va.iakovidis@gmail.com>
2025-04-08 11:02:03 +0300

    more filters

--- src/main/java/com/vaiak/moto_compare/repositories/specifications/MotoSpecs.java
+++ src/main/java/com/vaiak/moto_compare/repositories/specifications/MotoSpecs.java
@@ -1,5 +1,6 @@
 package com.vaiak.moto_compare.repositories.specifications;
 
+import com.vaiak.moto_compare.enums.Category;
 import com.vaiak.moto_compare.models.Motorcycle;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -8,5 +9,35 @@ public class MotoSpecs {
     public static Specification<Motorcycle> hasManufacturer(String manufacturer) {
         return ((root,query,criteriaBuilder) ->
             criteriaBuilder.equal(root.get("manufacturer"), manufacturer));
+    }
+
+    public static Specification<Motorcycle> isCategory(Category category) {
+        return ((root,query,criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("category"), category));
+    }
+
+    public static Specification<Motorcycle> hasSearchQuery(String search) {
+        return ((root,query,criteriaBuilder) ->
+                criteriaBuilder.like(criteriaBuilder.lower(root.get("model")), "%" + search.toLowerCase() + "%"));
+    }
+
+    public static Specification<Motorcycle> hasHorsePowerMin(int horsePowerMin) {
+        return ((root,query,criteriaBuilder) ->
+                criteriaBuilder.greaterThanOrEqualTo(root.get("horsePower"), horsePowerMin));
+    }
+
+    public static Specification<Motorcycle> hasHorsePowerMax(int horsePowerMax) {
+        return ((root,query,criteriaBuilder) ->
+                criteriaBuilder.lessThanOrEqualTo(root.get("horsePower"), horsePowerMax));
+    }
+
+    public static Specification<Motorcycle> hasDisplacementMin(int displacementMin) {
+        return ((root,query,criteriaBuilder) ->
+                criteriaBuilder.greaterThanOrEqualTo(root.get("displacement"), displacementMin));
+    }
+
+    public static Specification<Motorcycle> hasDisplacementMax(int displacementMax) {
+        return ((root,query,criteriaBuilder) ->
+                criteriaBuilder.lessThanOrEqualTo(root.get("displacement"), displacementMax));
     }
 }
