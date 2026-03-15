0f047158ed38ccd100b3223f51373b3efae02309
Vasileios Iakovidis <32614900+iakovidva@users.noreply.github.com>
2026-03-15 21:05:41 +0100

    Remove dummy failing test case
    Removed dummy failing test from MotoCompareApplicationTests.

--- src/test/java/com/vaiak/moto_compare/MotoCompareApplicationTests.java
+++ src/test/java/com/vaiak/moto_compare/MotoCompareApplicationTests.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootTest
 @ActiveProfiles("test")
 class MotoCompareApplicationTests {
@@ -13,9 +11,4 @@ class MotoCompareApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
-    @Test
-    void dummyFailingTest() {
-        assertTrue(false);
-    }
 }
