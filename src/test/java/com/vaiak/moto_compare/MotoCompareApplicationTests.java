00fa6cae4352716861eb65cb910d2b4df42c9bbb
vasileiosiakovidis <va.iakovidis@gmail.com>
2026-03-15 20:46:35 +0100

    fail publish if there is failing test

--- src/test/java/com/vaiak/moto_compare/MotoCompareApplicationTests.java
+++ src/test/java/com/vaiak/moto_compare/MotoCompareApplicationTests.java
@@ -2,12 +2,20 @@ package com.vaiak.moto_compare;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MotoCompareApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+    @Test
+    void dummyFailingTest() {
+        assertTrue(false);
+    }
 }
